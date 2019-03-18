b3fa738367b680917c8772a3a1130ac574df102e
Takashi Oda <wiwi5223@gmail.com>
2019-03-18 17:03:13 +0900

    host www.donosake.com & name&pass to ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sake_v0_#{Rails.env}"
   config.action_mailer.default_url_options = { :host => 'https://www.donosake.com' }
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
       :address => "smtp.sendgrid.net",
       :port => 587,
       :domain => "donosake.com",
-      :user_name => "app127775943@heroku.com",
-      :password => "sf6wruxt1671",
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
       :authentication => :plain
   }
   # Ignore bad email addresses and do not raise email delivery errors.

fa4ea0481802b6307a7bf15407f6be4402d090ca
Takashi Oda <wiwi5223@gmail.com>
2019-03-18 18:38:02 +0900

    test gmail on pro

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@donosake.com'
+  default from: 'wiwi5223@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
-      :address => "smtp.sendgrid.net",
+      :address => "smtp.gmail.com",
       :port => 587,
-      :domain => "donosake.com",
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => "gmail.com",
+      :user_name => "wiwi5223@gmail.com",
+      :password => "prqffoofrdgwjkdq",
       :authentication => :plain
   }
   # Ignore bad email addresses and do not raise email delivery errors.

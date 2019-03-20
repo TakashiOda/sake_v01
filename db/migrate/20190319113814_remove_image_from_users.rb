418270f8c8f0f6858189c815632ac4e6b9061253
Takashi Oda <wiwi5223@gmail.com>
2019-03-20 20:21:36 +0900

    delete migration file remove

--- db/migrate/20190319113814_remove_image_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveImageFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :profile_image, :string
-    remove_column :users, :profile_text, :text
-    remove_column :users, :business_type, :string
-  end
-end

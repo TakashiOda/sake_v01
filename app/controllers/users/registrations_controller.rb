ba812a0183b8a36418ffc85e310a3d3973c58934
Takashi Oda <wiwi5223@gmail.com>
2019-03-22 11:51:49 +0900

    fix signup url error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,31 +5,32 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   build_resource(sign_up_params)
-  #   resource.save
-  #   yield resource if block_given?
-  #   if resource.persisted?
-  #     if resource.active_for_authentication?
-  #       set_flash_message! :notice, :signed_up
-  #       sign_up(resource_name, resource)
-  #       respond_with resource, location: after_sign_up_path_for(resource)
-  #     else
-  #       set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-  #       expire_data_after_sign_in!
-  #       respond_with resource, location: after_inactive_sign_up_path_for(resource)
-  #     end
-  #   else
-  #     # clean_up_passwords resource
-  #     # set_minimum_password_length
-  #     render 'registrations/new'
-  #   end
-  # end
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      # binding.pry
+      # clean_up_passwords resource
+      # set_minimum_password_length
+      redirect_to new_user_registration_path, alert: 'メールアドレスが有効ではありません'
+    end
+  end
 
 
   # GET /resource/edit
